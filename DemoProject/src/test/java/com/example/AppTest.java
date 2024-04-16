d341883e4691f156a8b4e3bab2c92bc00baaaa58
ashishgitrepos <ashishrmalviya@gmail.com>
2024-04-16 15:39:14 +0530

    Merge conflict resolution

---
+++ DemoProject/src/test/java/com/example/AppTest.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.testng.annotations.Test;
+
+public class AppTest
+{
+    @Test
+    public void method1()
+    {
+        System.out.println( "TestApp111" );
+    }
+    @Test
+    public void method2()
+    {
+        System.out.println( "TestApp222" );
+    }
+}
---
+++ demo/src/test/java/auto/Javafile.java
@@ -0,0 +1,12 @@
+package test.java.auto;
+
+public class Javafile {
+
+    public static void main(String[] args) {
+        System.out.println("HHHHHHHHHHHHHHHHHHH");
+    }
+    @Test
+    public void method(){
+        System.out.println("WWIIMMJJHYYY");
+    }
+}
